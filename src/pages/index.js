c7520dd1ae4d56f3693016fc971f35ca89b3208a
runar <runar@tuta.io>
2020-03-08 12:03:19 +0100

    Display projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from "react"
 import { Link, graphql } from "gatsby"
-import {css} from '@emotion/core'
+import { css } from "@emotion/core"
+import { groupBy } from "lodash"
 
 const BlogIndex = ({ data }) => {
   const { edges: posts } = data.allMdx
@@ -8,22 +9,79 @@ const BlogIndex = ({ data }) => {
   console.log("mdPosts", mdPosts)
   const allPosts = [...posts, ...mdPosts]
 
+  const groupedPosts = groupBy(
+    allPosts,
+    ({ node: post }) => post.frontmatter.type
+  )
+  // {allPosts.map(({ node: post }) => (
   return (
-    <div css={css`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    align-content: flex-end;
-    `}>
-      <h1>Runars blog </h1>
-        {allPosts.map(({ node: post }) => (
-          <div key={post.id}>
-            <Link to={post.fields.slug}>
-              <h2>{post.frontmatter.title}</h2>
-            </Link>
-            <p>{post.excerpt}</p>
-          </div>
-        ))}
+    <div>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+        `}
+      >
+        <h1>Runars homepage </h1>
+      </div>
+      <div
+        css={css`
+          display: flex;
+        `}
+      >
+        <div
+          css={css`
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <h2
+            css={css`
+              align-self: center;
+            `}
+          >
+            Blog
+          </h2>
+          {groupedPosts["personal"].map(({ node: post }) => (
+            <div key={post.id}>
+              <Link to={post.fields.slug}>
+                <h2>{post.frontmatter.title}</h2>
+              </Link>
+              <p>{post.excerpt}</p>
+            </div>
+          ))}
+          {groupedPosts["blog"].map(({ node: post }) => (
+            <div key={post.id}>
+              <Link to={post.fields.slug}>
+                <h2>{post.frontmatter.title}</h2>
+              </Link>
+              <p>{post.excerpt}</p>
+            </div>
+          ))}
+        </div>
+        <div
+          css={css`
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <h2
+            css={css`
+              align-self: center;
+            `}
+          >
+            Projects
+          </h2>
+          {groupedPosts["project"].map(({ node: post }) => (
+            <div key={post.id}>
+              <Link to={post.fields.slug}>
+                <h2>{post.frontmatter.title}</h2>
+              </Link>
+              <p>{post.excerpt}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
@@ -32,33 +90,35 @@ export default BlogIndex
 
 export const pageQuery = graphql`
   query blogIndex {
-  allMdx(sort: { order: DESC, fields: frontmatter___date }) {
-    edges {
-      node {
-        id
-        excerpt
-        frontmatter {
-          title
+    allMdx(sort: { order: DESC, fields: frontmatter___date }) {
+      edges {
+        node {
+          id
+          excerpt
+          frontmatter {
+            title
+            type
+          }
+          fields {
+            slug
+          }
         }
-        fields {
-          slug
+      }
+    }
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          excerpt
+          frontmatter {
+            title
+            type
+          }
+          fields {
+            slug
+          }
         }
       }
     }
   }
-  allMarkdownRemark {
-    edges {
-      node {
-        id
-        excerpt
-        frontmatter {
-          title
-        }
-        fields {
-          slug
-        }
-      }
-    }
-  }
-}
 `
