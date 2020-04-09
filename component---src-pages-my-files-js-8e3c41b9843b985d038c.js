f64f01b2f1890408f22728849c508b92f6884afe
Travis CI User <travis@example.org>
2020-04-09 11:15:52 +0000

    Updates

--- component---src-pages-my-files-js-8e3c41b9843b985d038c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"9Gez":function(n,e,t){"use strict";t.r(e),t.d(e,"query",(function(){return a}));t("q1tI");var r=t("qKvR");e.default=function(n){var e=n.data;return console.log(e),Object(r.a)("div",null,e.allFile.edges.map((function(n,e){var t=n.node;return Object(r.a)("div",{key:e},JSON.stringify(t))})))};var a="3953592347"}}]);
-//# sourceMappingURL=component---src-pages-my-files-js-8e3c41b9843b985d038c.js.map
